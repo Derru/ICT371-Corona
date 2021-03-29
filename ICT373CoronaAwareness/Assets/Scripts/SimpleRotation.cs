bfa658a07dbe5a9b4a955c776a41d4ffbbbc4bf4
naza332 <55831271+naza332@users.noreply.github.com>
2021-03-29 22:48:26 +0800

    Minor Changes
    29/03/2021
    Nazier
    
    Just removed the mesh colliders causing errors (they weren't needed as we are using box colliders anyway).
    
    Spiced up MainMenu Scene a bit.

---
+++ ICT373CoronaAwareness/Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(10, 5, 20) * Time.deltaTime);
+    }
+}
